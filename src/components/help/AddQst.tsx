9840e7f4ad3a12b00e15ff624dbdb2ecbcacc67a
chiheb rahmouni <chihebrahmouni30@gmail.com>
2024-11-17 11:27:58 +0100

    added add help qsts

--- src/components/help/AddQst.tsx
+++ src/components/help/AddQst.tsx
@@ -1,91 +1,190 @@
 import React, { useState } from "react";
 import ReactModal from "react-modal";
-import { TextField, Button, Box, Typography } from "@mui/material";
+import { TextField, Button, Box, Typography, Select, MenuItem } from "@mui/material";
 import { Editor } from "@tinymce/tinymce-react";
 
+
 interface AddQstProps {
-    setClose: (isOpen: boolean) => void;
+  setClose: (isOpen: boolean) => void;
 }
 
 ReactModal.setAppElement("#root");
 
+const categoriesArray = ["All", "About", "Features", "Listings", "Regions", "Users"];
+
 const AddQst: React.FC<AddQstProps> = ({ setClose }) => {
-    const [question, setQuestion] = useState("");
-    const [answer, setAnswer] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [category, setCategory] = useState("");
+  const mainColor = "#FF385C";
+  // const [categoriesArray, setCategoriesArray] = useState<string[]>([]);
+  console.log(answer);
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        // Handle the form submission logic here
-        console.log("Question:", question);
-        console.log("Answer:", answer);
-        setClose(false);
-    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle the form submission logic here
+    console.log("Question:", question);
+    console.log("Answer:", answer);
+    setClose(false);
+  };
 
-    return (
-      <ReactModal
-        isOpen={true}
-        onRequestClose={() => setClose(false)}
-        className={"bg-white rounded-lg p-4 shadow-hardShadow lg:p-6"}
-        overlayClassName={
-          "fixed bg-black bg-opacity-10 backdrop-blur-[7px] inset-0 flex items-center justify-center p-4 mt-[60px] lg:mt-[80px]"
-        }
-      >
-        <Typography variant="h4" component="h2" gutterBottom>
-          Add Question
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <Box mb={2}>
-            <TextField
-              label="Question"
-              variant="outlined"
-              fullWidth
-              value={question}
-              onChange={(e) => setQuestion(e.target.value)}
-            />
-          </Box>
-          <Box mb={2}>
-            <Editor
-              apiKey="trrkyoh7hvt1k7rolyktoalj3j8u1234p1e3isyk9mdfuj4j"
-              initialValue="<p>Enter the answer here...</p>"
-              init={{
-                height: 300,
-                menubar: false,
-                plugins: [
-                  "advlist autolink lists link image charmap print preview anchor",
-                  "searchreplace visualblocks code fullscreen",
-                  "insertdatetime media table paste code help wordcount",
-                ],
-                toolbar:
-                  "undo redo | formatselect | bold italic backcolor | \
-                                alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | removeformat | help",
-              }}
-              onEditorChange={(content) => setAnswer(content)}
-            />
-          </Box>
-          <Box display="flex" justifyContent="flex-end" gap={2}>
-            <Button
-              variant="outlined"
-              onClick={() => setClose(false)}
-              sx={{
-                color: "#FF385C",
-                backgroundColor: "white",
-                borderColor: "#FF385C",
-              }}
-            >
-              Cancel
-            </Button>
-            <Button
-              variant="contained"
-              sx={{ color: "white", backgroundColor: "#FF385C" }}
-              type="submit"
-            >
-              Add
-            </Button>
-          </Box>
-        </form>
-      </ReactModal>
-    );
+  return (
+    <ReactModal
+      isOpen={true}
+      onRequestClose={() => setClose(false)}
+      className={
+        "bg-white rounded-lg p-4 shadow-hardShadow lg:p-6 xl:max-w-[1000px] xl:mx-auto"
+      }
+      overlayClassName={
+        "fixed bg-black bg-opacity-10 backdrop-blur-[7px] inset-0 flex items-center justify-center p-4 mt-[60px] md:px-20 lg:mt-[80px] "
+      }
+    >
+      <Typography variant="h4" component="h2" gutterBottom>
+        Add Question
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4 flex gap-2">
+          <TextField
+            label="Question"
+            variant="outlined"
+            fullWidth
+            value={question}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "grey",
+                },
+                "&:hover fieldset": {
+                  borderColor: "grey",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: mainColor,
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: "gray",
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: mainColor,
+              },
+            }}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <Select
+            label="Question"
+            sx={{
+              width: 120,
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: mainColor,
+                },
+                "&:hover fieldset": {
+                  borderColor: mainColor,
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: mainColor,
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: "gray",
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: mainColor,
+              },
+              "& .MuiSelect-select": {
+                color: "black", 
+              },
+            }}
+            variant="outlined"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            displayEmpty
+          >
+            <MenuItem value="" disabled>
+              Select Question category
+            </MenuItem>
+            {categoriesArray.map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+        <Box mb={2} className="ckeditor-container">
+          <Editor
+            apiKey="trrkyoh7hvt1k7rolyktoalj3j8u1234p1e3isyk9mdfuj4j"
+            init={{
+              plugins: [
+                "anchor",
+                "autolink",
+                "charmap",
+                "codesample",
+                "emoticons",
+                "image",
+                "link",
+                "lists",
+                "media",
+                "searchreplace",
+                "table",
+                "visualblocks",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+              tinycomments_mode: "embedded",
+              tinycomments_author: "Author name",
+              mergetags_list: [
+                { value: "First.Name", title: "First Name" },
+                { value: "Email", title: "Email" },
+              ],
+              ai_request: (respondWith: any) =>
+                respondWith.string(() =>
+                  Promise.reject("See docs to implement AI Assistant")
+                ),
+              exportpdf_converter_options: {
+                format: "Letter",
+                margin_top: "1in",
+                margin_right: "1in",
+                margin_bottom: "1in",
+                margin_left: "1in",
+              },
+              exportword_converter_options: { document: { size: "Letter" } },
+              importword_converter_options: {
+                formatting: {
+                  styles: "inline",
+                  resets: "inline",
+                  defaults: "inline",
+                },
+              },
+            }}
+            initialValue="Answer..."
+            onEditorChange={(content) => setAnswer(content)}
+            // value={answer}
+          />
+        </Box>
+        <Box display="flex" justifyContent="flex-end" gap={2}>
+          <Button
+            variant="outlined"
+            onClick={() => setClose(false)}
+            sx={{
+              color: mainColor,
+              backgroundColor: "white",
+              borderColor: "#FF385C",
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ color: "white", backgroundColor: mainColor }}
+            type="submit"
+          >
+            Add
+          </Button>
+        </Box>
+      </form>
+    </ReactModal>
+  );
 };
 
 export default AddQst;
--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import Swal from "sweetalert2";
 import CatQsts from '../containers/help/CatQsts';
+import React from 'react';
 
 const Help = () => {
     const [selectedCat, setSelectedCat] = useState(helpCat[0].id);
+
 
     const handleCategoryClick = (id: number) => {
         setSelectedCat(id);
@@ -35,8 +37,15 @@ const Help = () => {
 
 const TableOfQuestions = ({ categoryId }: { categoryId: number }) => {
     const questions = getQuestionsByCategory(categoryId);
+        const [selectedQuestionId, setSelectedQuestionId] = useState<
+          number | null
+        >(null);
 
-    const handleDelete = (id: number) => {
+        const handleRowClick = (id: number) => {
+          setSelectedQuestionId(selectedQuestionId === id ? null : id);
+        };
+
+    const handleDelete = (_id: number) => {
         Swal.fire({
             title: "Are you sure?",
             text: "Do you want to delete this question?",
@@ -53,45 +62,98 @@ const TableOfQuestions = ({ categoryId }: { categoryId: number }) => {
     };
 
     return (
-        <div className="overflow-x-auto">
-            <table className="min-w-full bg-white">
-                <thead>
-                    <tr>
-                        <th className="py-2 border-b">Question</th>
-                        <th className="py-2 border-b">Answer</th>
-                        <th className="py-2 px-4 border-b">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {questions.map((question) => (
-                        <tr key={question.id} className="hover:bg-gray-100">
-                            <td className="border px-4 py-2 truncate max-w-xs">
-                                {question.question}
-                            </td>
-                            <td className="border px-4 py-2 truncate max-w-xs">
-                                <div dangerouslySetInnerHTML={{ __html: question.answer }} />
-                            </td>
-                            <td className="px-4 mt-2.5 flex space-x-2 justify-center">
-                                <button className="text-red-500" onClick={() => handleDelete(question.id)}>
-                                    <FaTrash />
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white">
+          <thead>
+            <tr>
+              <th className="py-2 border-b">Question</th>
+              <th className="py-2 px-4 border-b">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {questions.map((question) => (
+              <React.Fragment key={question.id}>
+                <tr
+                  className="hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleRowClick(question.id)}
+                >
+                  <td className="border px-4 py-2 truncate max-w-xs">
+                    {question.question}
+                  </td>
+                  <td className="px-4 mt-2.5 flex space-x-2 justify-center">
+                    <button
+                      className="text-red-500"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleDelete(question.id);
+                      }}
+                    >
+                      <FaTrash />
+                    </button>
+                  </td>
+                </tr>
+                {selectedQuestionId === question.id && (
+                  <tr>
+                    <td colSpan={2} className="border px-4 py-2">
+                      <div
+                        dangerouslySetInnerHTML={{ __html: question.answer }}
+                      />
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
 };
 
 const getQuestionsByCategory = (categoryId: number) => {
     const allQuestions = [
-        { id: 1, categoryId: 1, question: 'What is this?', answer: '<p class="text-main">This is a general question.</p>' },
-        { id: 2, categoryId: 1, question: 'What is this iuuegd  zoidyo zpz pU eue^*qefio àqufquf qq^fiuqiuqçzu z?', answer: '<p>This is a general question. mzuuzitdp</p>' },
-        { id: 3, categoryId: 1, question: 'What is this?', answer: '<p>This is a general question.</p>' },
-        { id: 4, categoryId: 2, question: 'How to create an account?', answer: '<p>Follow these steps...</p>' },
-        { id: 5, categoryId: 3, question: 'How to pay?', answer: '<p>You can pay using...</p>' },
-        { id: 6, categoryId: 4, question: 'How to subscribe?', answer: '<p>To subscribe, you need to...</p>' },
+      {
+        id: 1,
+        categoryId: 1,
+        question: "What is this?",
+        answer: '<p class="text-main">This is a general question.</p>',
+      },
+      {
+        id: 2,
+        categoryId: 1,
+        question:
+          "What is this iuuegd  zoidyo zpz pU eue^*qefio àqufquf qq^fiuqiuqçzu z?",
+        answer: "<p>This is a general question. mzuuzitdp</p>",
+      },
+      {
+        id: 3,
+        categoryId: 1,
+        question: "What is this?",
+        answer: "<p>This is a general question.</p>",
+      },
+      {
+        id: 4,
+        categoryId: 2,
+        question: "How to create an account?",
+        answer: `<h2><strong>The answer is:</strong></h2>
+<p>if u have any qsts:</p>
+<ol>
+<li>go there</li>
+<li>go pk</li>
+</ol>
+<p>deal</p>`,
+      },
+      {
+        id: 5,
+        categoryId: 3,
+        question: "How to pay?",
+        answer: "<p>You can pay using...</p>",
+      },
+      {
+        id: 6,
+        categoryId: 4,
+        question: "How to subscribe?",
+        answer: "<p>To subscribe, you need to...</p>",
+      },
     ];
 
     return allQuestions.filter((q) => q.categoryId === categoryId);
@@ -105,3 +167,153 @@ const helpCat = [
 ];
 
 export default Help;
+
+
+
+// import { useState } from 'react';
+// import { FaTrash } from 'react-icons/fa';
+// import Swal from "sweetalert2";
+// import CatQsts from '../containers/help/CatQsts';
+
+// const Help = () => {
+//     const [selectedCat, setSelectedCat] = useState(helpCat[0].id);
+
+//     const handleCategoryClick = (id: number) => {
+//         setSelectedCat(id);
+//     };
+
+//     return (
+//       <div className="p-4 md:p-8 lg:max-w-[1000px] mx-auto">
+//         <h1 className="text-3xl md:text-4xl font-extrabold mb-4 text">
+//           Help Questions Management
+//         </h1>
+//         <p className="text-sm md:text-base text-gray-600 mb-8">
+//           Explore and manage help questions with detailed insights into each
+//           query available for assistance.
+//         </p>
+
+//         {/* categories and add */}
+//         <CatQsts
+//           helpCat={helpCat}
+//           selectedCat={selectedCat}
+//           handleCategoryClick={handleCategoryClick}
+//         />
+//         <div>
+//           <TableOfQuestions categoryId={selectedCat} />
+//         </div>
+//       </div>
+//     );
+// };
+
+// const TableOfQuestions = ({ categoryId }: { categoryId: number }) => {
+//     const questions = getQuestionsByCategory(categoryId);
+
+//     const handleDelete = (_id: number) => {
+//         Swal.fire({
+//             title: "Are you sure?",
+//             text: "Do you want to delete this question?",
+//             icon: "warning",
+//             showCancelButton: true,
+//             confirmButtonText: "Yes, delete it!",
+//             cancelButtonText: "No, keep it",
+//         }).then((result) => {
+//             if (result.isConfirmed) {
+//                 // Perform the delete operation here
+//                 Swal.fire("Success", "The question has been deleted!", "success");
+//             }
+//         });
+//     };
+
+//     return (
+//       <div className="overflow-x-auto">
+//         <table className="min-w-full bg-white">
+//           <thead>
+//             <tr>
+//               <th className="py-2 border-b">Question</th>
+//               <th className="py-2 border-b">Answer</th>
+//               <th className="py-2 px-4 border-b">Actions</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {questions.map((question) => (
+//               <tr key={question.id} className="hover:bg-gray-100">
+//                 <td className="border px-4 py-2 truncate max-w-xs">
+//                   {question.question}
+//                 </td>
+//                 <td className="border px-4 py-2 truncate max-w-xs">
+//                   <div dangerouslySetInnerHTML={{ __html: question.answer }} />
+//                 </td>
+//                 <td className="px-4 mt-2.5 flex space-x-2 justify-center">
+//                   <button
+//                     className="text-red-500"
+//                     onClick={() => handleDelete(question.id)}
+//                   >
+//                     <FaTrash />
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+// };
+
+// const getQuestionsByCategory = (categoryId: number) => {
+//     const allQuestions = [
+//       {
+//         id: 1,
+//         categoryId: 1,
+//         question: "What is this?",
+//         answer: '<p class="text-main">This is a general question.</p>',
+//       },
+//       {
+//         id: 2,
+//         categoryId: 1,
+//         question:
+//           "What is this iuuegd  zoidyo zpz pU eue^*qefio àqufquf qq^fiuqiuqçzu z?",
+//         answer: "<p>This is a general question. mzuuzitdp</p>",
+//       },
+//       {
+//         id: 3,
+//         categoryId: 1,
+//         question: "What is this?",
+//         answer: "<p>This is a general question.</p>",
+//       },
+//       {
+//         id: 4,
+//         categoryId: 2,
+//         question: "How to create an account?",
+//         answer: `<h2><strong>The answer is:</strong></h2>
+// <p>if u have any qsts:</p>
+// <ol>
+// <li>go there</li>
+// <li>go pk</li>
+// </ol>
+// <p>deal</p>`,
+//       },
+//       {
+//         id: 5,
+//         categoryId: 3,
+//         question: "How to pay?",
+//         answer: "<p>You can pay using...</p>",
+//       },
+//       {
+//         id: 6,
+//         categoryId: 4,
+//         question: "How to subscribe?",
+//         answer: "<p>To subscribe, you need to...</p>",
+//       },
+//     ];
+
+//     return allQuestions.filter((q) => q.categoryId === categoryId);
+// };
+
+// const helpCat = [
+//     { id: 1, name: 'General' },
+//     { id: 2, name: 'Account' },
+//     { id: 3, name: 'Billing' },
+//     { id: 4, name: 'Subscription' },
+// ];
+
+// export default Help;
